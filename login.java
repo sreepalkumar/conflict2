810c64530e85e7cbc94c1aef78e3fe2a6d1b3dc4
sreepalkumar <sreepal33333@gmail.com>
2023-08-31 12:05:49 +0530

    i want 20

--- login.java
+++ login.java
@@ -3,13 +3,11 @@ public class login.y
    puiblic static void main(string args[])
 {
 int i;
-for(i=0;i<=10;i++){
+for(i=0;i<=20;i++){
 system.out.println(number:" +i);
 }
 }
 }
-a=10
-b=20
-c=30
+
 
 
